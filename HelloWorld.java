4c6c094b03db6cc659b371858a1d7c9186c28d30
missy1020 <82297498+missy1020@users.noreply.github.com>
2026-09-20 02:05:52 +0200

    2026 - Hello from IntelliJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 ﻿public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from VSCode 2026!");
+        System.out.println("Hello from IntelliJ 2026!");
     }
 }
