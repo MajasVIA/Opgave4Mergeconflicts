af737efb7261de36a0bed7a0bd152a20de589293
missy1020 <82297498+missy1020@users.noreply.github.com>
2026-09-20 02:42:21 +0200

    2026 - Konfliktændring fra IntelliJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 ﻿public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from IntelliJ 2026!");
+        System.out.println("Ændring fra IntelliJ 2026, som skaber konflikt!");
     }
 }
