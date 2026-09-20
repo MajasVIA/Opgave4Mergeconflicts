5745139cc997b495dc9b84ca8cfbfff9ce462b2b
missy1020 <82297498+missy1020@users.noreply.github.com>
2026-09-20 02:02:16 +0200

    2026 - Hello from VSCode

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class HelloWorld {
+﻿public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Ændring fra VSCode, som skaber konflikt!");
+        System.out.println("Hello from VSCode 2026!");
     }
 }
