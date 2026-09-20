7f1e4335f72258029815aedcf5353d1213c2ad3f
missy1020 <82297498+missy1020@users.noreply.github.com>
2026-09-20 02:31:36 +0200

    2026 - Konfliktændring fra VSCode

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 ﻿public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from VSCode 2026!");
+        System.out.println("Ændring fra VSCode 2026, som skaber konflikt!");
     }
 }
